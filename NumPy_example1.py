915d0b02c4590e5dc95fc04678960b4517758fee
SARDAR OMRANI <110787623+sardaromrani1@users.noreply.github.com>
2024-05-02 22:59:26 +0330

    Add files via upload
    NumPy example

---
+++ NumPy_example1.py
@@ -0,0 +1,11 @@
+# NumPy Array Slicing
+
+import numpy as np
+
+# Create a 2D array
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# Selecting a subset of the array
+subset = arr[0:2, 1:3]
+
+print(subset)
